2339ce8aee68f23e89814c6d7cfabbb8e6cc7647
Samuel Muniz <137716288+DevSamuelMuniz@users.noreply.github.com>
2025-01-09 00:39:42 -0300

    post timeline

--- senac-eventos-app/app/components/timeline/cardTimeline/page.tsx
+++ senac-eventos-app/app/components/timeline/cardTimeline/page.tsx
@@ -1,42 +1,63 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import { fetchPosts } from "@/app/services/postService";
 
 export default function CardTimeline() {
-  const [liked, setLiked] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [likedPosts, setLikedPosts] = useState<{ [key: number]: boolean }>({});
 
-  const handleLike = () => {
-    setLiked(!liked);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchPosts();
+        setPosts(data);
+      } catch (error) {
+        console.error("Erro ao buscar posts:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleLike = (id: number) => {
+    setLikedPosts((prev) => ({ ...prev, [id]: !prev[id] }));
   };
 
   return (
-    <main className="max-w-sm rounded-lg shadow-md bg-white overflow-hidden">
-      <img
-        src="https://media.istockphoto.com/id/499517325/pt/foto/um-homem-falando-em-uma-confer%C3%AAncia-de-neg%C3%B3cios.jpg?s=612x612&w=0&k=20&c=yxUfXB3YGNK9trUiaFJrOJBQxrDzYLfTynw-2OHt-ys="
-        alt="Conferência de Negócios"
-        className="w-full h-72 object-cover"
-      />
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      {posts.map((post) => (
+        <main
+          key={post.idPost}
+          className="max-w-sm rounded-lg shadow-md bg-white overflow-hidden"
+        >
+          <img
+            src={post.imageUrl}
+            alt="Imagem do Post"
+            className="w-full h-72 object-cover"
+          />
 
-      <div className="p-4">
-        <h1 className="text-xl font-bold mb-2 text-gray-700">Título</h1>
-        <p className="text-gray-700 mb-4">
-          Descrição do conteúdo. Aqui vai uma breve explicação sobre o tópico.
-        </p>
+          <div className="p-4">
+            <h1 className="font-semibold mb-2 text-gray-700">
+              {post.description}
+            </h1>
+            <p className="text-gray-700 mb-4">Autor: {post.participant.name} ( {post.participant.companyName})</p>
 
-        <div className="flex items-center space-x-2">
-          <button
-            onClick={handleLike}
-            className="focus:outline-none transition-transform transform hover:scale-110"
-          >
-            {liked ? (
-              <FavoriteIcon className="text-red-500" />
-            ) : (
-              <FavoriteBorderIcon className="text-gray-400" />
-            )}
-          </button>
-          <span className="text-gray-700 font-medium">Curtir</span>
-        </div>
-      </div>
-    </main>
+            <div className="flex items-center space-x-2">
+              <button
+                onClick={() => handleLike(post.idPost)}
+                className="focus:outline-none transition-transform transform hover:scale-110"
+              >
+                {likedPosts[post.idPost] ? (
+                  <FavoriteIcon className="text-red-500" />
+                ) : (
+                  <FavoriteBorderIcon className="text-gray-400" />
+                )}
+              </button>
+              <span className="text-gray-700 font-medium">Curtir</span>
+            </div>
+          </div>
+        </main>
+      ))}
+    </div>
   );
 }
--- senac-eventos-app/app/timeline/page.tsx
+++ senac-eventos-app/app/timeline/page.tsx
@@ -7,11 +7,11 @@ import CardTimeline from "../components/timeline/cardTimeline/page";
 const TimelinePage = () => {
   return (
     <main>
-      <header className="flex justify-center bg-blue-50 p-14">
+      <header className="flex justify-center bg-blue-50 p-14 fixed w-full top-0 z-50">
         <h1 className="text-2xl font-bold text-blue-300">Linha do Tempo</h1>
       </header>
 
-      <div className="m-4 flex flex-col gap-6">
+      <div className="m-4 flex flex-col gap-6 items-center py-4 pt-36">
         <CardTimeline/>
         <CardTimeline/>
         <CardTimeline/>
